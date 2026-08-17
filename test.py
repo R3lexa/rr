d69a3b125cc587742025f37411f19d07a543e9fe
Qunik <160403797+R3lexa@users.noreply.github.com>
2026-08-18 01:43:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import socket, time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-target = ("69.46.46.125", 8080)
+target = ("69.46.46.64", 443)
 end = time.time() + 30
 while time.time() < end:
     sock.sendto(b"test", target)
